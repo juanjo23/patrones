3065fb90941a0c2724065fccafb83fa71751bab6
Juan José Miramontes Sandoval <juanjo_23@live.com>
2016-07-19 00:46:53 -0500

    Update settings.py
    Changing mysql password

--- patrones/settings.py
+++ patrones/settings.py
@@ -34,7 +34,7 @@ DATABASES = {
         'NAME': 'patrones',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
